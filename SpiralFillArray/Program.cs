761e4ff1707a4d104e10a932217b7065c1710916
Ksenya <ksyuhacr7@gmail.com>
2022-12-29 15:06:49 +0300

    done

--- SpiralFillArray/Program.cs
+++ SpiralFillArray/Program.cs
@@ -1,1 +1,33 @@
-﻿int [,] matrix = new int [4,4];
+﻿int n = 4;
+int [,] matrix = new int [n,n];
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(matrix);
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
